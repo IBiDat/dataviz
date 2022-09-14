176b1e8a0ab9de6052f4f5bd3b086874defa6cff
Iñaki Úcar <iucar@fedoraproject.org>
2022-09-14 17:24:07 +0200

    add chapter 1

--- slides/assets/macros.js
+++ slides/assets/macros.js
@@ -1,3 +1,23 @@
+// automagic toc: use .intoc to add h1 to toc, .toc to display
+document.addEventListener('DOMContentLoaded', function(event) {
+  var toc = document.getElementsByClassName('toc')[0];
+  if (toc === undefined) return;
+  var intoc = document.getElementsByClassName('intoc');
+  var n = 0;
+  var ul = document.createElement('ul');
+  intoc.forEach(x => {
+    let a = document.createElement('a');
+    let i = parseInt(x.getElementsByClassName('remark-slide-number')[0].textContent);
+    if (i <= n) return; n = i;
+    a.href = "#" + n;
+    a.textContent = x.getElementsByTagName('h1')[0].textContent;
+    let li = document.createElement('li');
+    li.appendChild(a);
+    ul.appendChild(li);
+  })
+  toc.appendChild(ul);
+});
+
 remark.macros.vspace = function(px) {
   return '<div style="height: ' + px + 'px"></div>';
 }
