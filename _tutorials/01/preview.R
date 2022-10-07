f7bb42cd107b80281c579c4b19545d92c0fe312f
Iñaki Úcar <iucar@fedoraproject.org>
2022-10-07 15:16:16 +0200

    improve abstract

--- _tutorials/01/preview.R
+++ _tutorials/01/preview.R
@@ -6,4 +6,4 @@ p3 <- p2 + geom_point()
 p4 <- p3 + geom_point(aes(color=class))
 
 p <- patchwork::wrap_plots(p1, p2, p3, p4)
-ggsave("_tutorials/01/preview.png", p, width=7, height=3.5, dpi=150)
+ggsave("_tutorials/01/preview.png", p, width=7, height=4, dpi=150)
