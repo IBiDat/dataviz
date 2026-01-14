cc1cc0c36ef27a358c0eb1624ce20b7601ffa6cc
Iñaki Úcar <iucar@fedoraproject.org>
2026-01-14 15:11:35 +0100

    rebuild and some cleanup

--- _projects/2025/100453214/setup.R
+++
@@ -1,7 +0,0 @@
-distill::create_post(
-  title = "Draft Project",
-  collection = paste0("projects/", format(Sys.Date(), "%Y")),
-  author = "María Garcés Blázquez",
-  slug = 100453214,
-  date_prefix = NULL
-)
